2a26c1c875050611e23074cca9ac8a1f5e36334c
basslines <admir.osmanovic@gmail.com>
2021-03-10 15:26:38 +0100

    dodao index.js fajl

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    // Admir Osmanovic
+    /* admir
+    osmanovic
+    bassline
+    */
+    // Only change code above this line
+}
+console.log(main());
+module.exports = main;
